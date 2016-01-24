b48d34111b71345b4db7be8da968848574940fd0
Łukasz Łobocki <l.lobocki@gmail.com>
2016-01-24 13:45:27 +0100

    complete

---
+++ plot2.R
@@ -0,0 +1,10 @@
+Sys.setlocale("LC_TIME", "en_US.UTF-8")
+
+csv <- read.csv("household_power_consumption.txt", sep=';', na.strings = "?", stringsAsFactors = FALSE)
+df <- subset(csv, csv$Date == '1/2/2007' | csv$Date == '2/2/2007')
+df$DateTime <- strptime(paste(df$Date, df$Time), "%d/%m/%Y%H:%M:%S")
+png(filename='plot2.png')
+plot(df$DateTime, df$Global_active_power, xlab="",
+     ylab="Global Active Power (kilowats)", type="n")
+lines(df$DateTime, df$Global_active_power)
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,14 @@
+Sys.setlocale("LC_TIME", "en_US.UTF-8")
+
+csv <- read.csv("household_power_consumption.txt", sep=';', na.strings = "?", stringsAsFactors = FALSE)
+df <- subset(csv, csv$Date == '1/2/2007' | csv$Date == '2/2/2007')
+df$DateTime <- strptime(paste(df$Date, df$Time), "%d/%m/%Y%H:%M:%S")
+png(filename='plot3.png')
+plot(df$DateTime, df$Sub_metering_1, xlab="",
+     ylab="Energy sub metering", type="n")
+lines(df$DateTime, df$Sub_metering_1, col="black")
+lines(df$DateTime, df$Sub_metering_2, col="red")
+lines(df$DateTime, df$Sub_metering_3, col="blue")
+legend('topright', c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3") , 
+       lty=1, col=c('black', "red", 'blue'), cex=.75)
+dev.off()
